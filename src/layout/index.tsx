60a196a6829ac99a4a68740e434e80aa44a29826
forest <alexeidorman@gmail.com>
2024-02-11 22:13:54 -0800

    sider style and collapse insert

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,21 @@
 import Sider from "antd/es/layout/Sider";
 import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import { useState } from "react";
 
 const BasicLayout: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider></Sider>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+        }}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      ></Sider>
       <Layout className="site-layout">
         <Header></Header>
         <Content></Content>
