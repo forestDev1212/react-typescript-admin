93e8db07dd292f37770007da02ec7a1c0a809a7c
forest <alexeidorman@gmail.com>
2024-02-11 22:00:11 -0800

    basiclayout file add

---
+++ .vite/deps/react.js
@@ -0,0 +1,3 @@
+import { require_react } from "./chunk-KJPXGTST.js";
+export default require_react();
+//# sourceMappingURL=react.js.map
---
+++ src/layout/index.tsx
@@ -0,0 +1,5 @@
+const BasicLayout: React.FC = () => {
+  return <p>This is Basic Layout</p>;
+};
+
+export default BasicLayout;
