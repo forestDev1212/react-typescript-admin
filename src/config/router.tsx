7d3f2574cf804991fa818cd8a25d2df62349173f
forest <alexeidorman@gmail.com>
2024-02-11 22:53:34 -0800

    route.tsx update 1

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import App from "../App";
+import {
+  DashboardOutlined,
+  EditOutlined,
+  TableOutlined,
+} from "@ant-design/icons";
 import { createBrowserRouter, Navigate } from "react-router-dom";
+import Dashboard from "../pages/Dashboard/index";
+import FormPage from "../pages/FormPage/index";
+import TablePage from "../pages/TablePage/index";
 
 const routes = [
   {
@@ -10,7 +18,22 @@ const routes = [
       {
         children: [
           {
-            path: "/",
+            index: true,
+            title: "Dashboard",
+            icon: <DashboardOutlined />,
+            element: <Dashboard />,
+          },
+          {
+            path: "form",
+            title: "表单页",
+            icon: <EditOutlined />,
+            element: <FormPage />,
+          },
+          {
+            path: "table",
+            title: "列表页",
+            icon: <TableOutlined />,
+            element: <TablePage />,
           },
         ],
       },
