cb3d74cb37d2cce980f2b6b63713cf0057421047
forest <alexeidorman@gmail.com>
2024-02-16 01:09:29 -0800

    aa

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -4,12 +4,14 @@ import {
   DashboardOutlined,
   EditOutlined,
   TableOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { authLoader } from "../App";
 import Dashboard from "../pages/Dashboard/index";
 import FormPage from "../pages/FormPage/index";
 import TablePage from "../pages/TablePage/index";
+import EmployeePage from "../pages/Employee";
 
 const routes = [
   {
@@ -36,6 +38,12 @@ const routes = [
             title: "列表页",
             icon: <TableOutlined />,
             element: <TablePage />,
+          },
+          {
+            path: "employee",
+            title: "工程师",
+            icon: <UserOutlined />,
+            element: <EmployeePage />,
           },
         ],
       },
--- src/pages/Employee/index.tsx
+++ src/pages/Employee/index.tsx
@@ -1,8 +1,59 @@
-import React from "react";
+import { ActionType, ProColumns, ProTable } from "@ant-design/pro-components";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+type Employee = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  // emailId : string;
+};
+
+const columns: ProColumns<Employee>[] = [
+  {
+    dataIndex: "firstName",
+    width: "20%",
+  },
+  {
+    dataIndex: "lastName",
+    width: "20%",
+  },
+  {
+    dataIndex: "emailId",
+    width: "20%",
+  },
+];
 const EmployeePage: React.FC = () => {
+  const actionRef = useRef<ActionType>();
+  const [dataSource, setDataSource] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/v1/employees").then((res) => {
+      console.log(res);
+      setDataSource(res.data);
+    });
+  }, []);
   return (
     <React.Fragment>
-      <p>This is Employee Page</p>
+      <ProTable<Employee>
+        columns={columns}
+        // request={dataSource}
+        // dataSource={dataSource}
+        request={async () => {
+          const res = await axios.get("http://localhost:8080/api/v1/employees");
+          // .then(res => {
+          //   return {
+          //     data : res.data,
+          //     success : true
+          //   }
+          // })
+          return {
+            data: res.data,
+            success: true,
+          };
+        }}
+        rowKey={(record) => record.id}
+      />
     </React.Fragment>
   );
 };
+
+export default EmployeePage;
